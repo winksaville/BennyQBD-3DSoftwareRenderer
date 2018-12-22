31ea8ebe11614257d31eda4ef5ee769e00073316
Wink Saville <wink@saville.com>
2018-12-21 21:14:05 -0800

    Add Entity and Movement

---
+++ src/Entity.java
@@ -0,0 +1,101 @@
+import java.awt.event.KeyEvent;
+
+public class Entity
+{
+	private Mesh m_mesh;
+	private Movement m_movement;
+
+	// Entity with Rotation and Translation Input
+	//
+	// translationKey = Shift
+	// rotationKey = control
+	//
+	//                 (k)
+	//                 +y     (i)
+	//                  |    -z
+	//                  |    /
+	//                  |   /
+	//                  |  /
+	//                  | /
+	//                  |/
+	// (h) -x ----------o---------- +x (l)
+	//                 /|
+	//                / |
+	//               /  |
+	//              /   |
+	//             /    |
+	//           +z     |
+	//          (m)    -y
+	//                 (j)
+	// 
+	public Entity(Mesh mesh, Vector4f position, Vector4f lookAtPoint, Vector4f up)
+	{
+		this(mesh, position, lookAtPoint, up,
+			KeyEvent.VK_L, KeyEvent.VK_K, KeyEvent.VK_M,
+			KeyEvent.VK_H, KeyEvent.VK_J, KeyEvent.VK_I,
+			KeyEvent.VK_SHIFT, KeyEvent.VK_CONTROL);
+	}
+
+	// Entity with Rotation and Translation Input
+	//
+	// rotationKey
+	// translationKey
+	//
+	//                 (yPlusKey)
+	//                 +y     (zNegKey)
+	//                  |    -z
+	//                  |    /
+	//                  |   /
+	//                  |  /
+	//                  | /
+	// (xNegKey)        |/
+	//     -x ----------o---------- +x (yPlusKey)
+	//                 /|
+	//                / |
+	//               /  |
+	//              /   |
+	//             /    |
+	//           +z     |
+	//    (zPlusKey)   -y
+	//                 (yNegKey)
+	// 
+	public Entity(Mesh mesh, Vector4f position, Vector4f lookAtPoint, Vector4f up,
+			int xPlusKey, int yPlusKey, int zPlusKey,
+			int xNegKey,  int yNegKey,  int zNegKey,
+			int translateKey, int rotateKey)
+	{
+		Dbg.p(String.format("x+=%d y+=%d z+=%d\n", xPlusKey, yPlusKey, zPlusKey));
+		Dbg.p(String.format("x-=%d y-=%d z-=%d\n", xNegKey, yNegKey, zNegKey));
+		Dbg.p(String.format("tr=%d ro=%d\n", translateKey, rotateKey));
+		m_mesh = mesh;
+		m_movement = new Movement(position, lookAtPoint, up,
+			xPlusKey, yPlusKey, zPlusKey,
+			xNegKey, yNegKey, zNegKey,
+			translateKey, rotateKey);
+	}
+
+	public void Update(Input input, float translationDelta, float rotationDelta)
+	{
+		m_movement.Update(input, translationDelta, rotationDelta);
+	}
+
+	public Mesh GetMesh()
+	{
+		return m_mesh;
+	}
+
+	public Transform GetTransform()
+	{
+		return m_movement.GetTransform();
+	}
+
+	public void Translate(Vector4f dir, float amt)
+	{
+		m_movement.Translate(dir, amt);
+	}
+
+	public void Rotate(Vector4f axis, float angle)
+	{
+		m_movement.Rotate(axis, angle);
+	}
+}
---
+++ src/Movement.java
@@ -0,0 +1,201 @@
+import java.awt.event.KeyEvent;
+
+class Movement {
+
+	final boolean DBG = false;
+
+	private Vector4f X_AXIS = new Vector4f(1, 0, 0);
+	private Vector4f Y_AXIS = new Vector4f(0, 1, 0);
+	private Vector4f Z_AXIS = new Vector4f(0, 0, 1);
+
+	private class KeyInfo {
+		boolean key1Pressed;
+		int key1;
+		boolean key2Pressed;
+		int key2;
+
+		KeyInfo(int key1, int key2) {
+			this.key1Pressed = false;
+			this.key1 = key1;
+			this.key2Pressed = false;
+			this.key2 = key2;
+		}
+	};
+
+	private KeyInfo m_keys[] = new KeyInfo[12];
+	private boolean m_changed;
+	private Transform m_transform;
+
+	private final int X_TRANS_PLUS = 0;
+	private final int Y_TRANS_PLUS = 1;
+	private final int Z_TRANS_PLUS = 2;
+	private final int X_TRANS_NEG  = 3;
+	private final int Y_TRANS_NEG  = 4;
+	private final int Z_TRANS_NEG  = 5;
+
+	private final int X_ROTATE_PLUS = 6;
+	private final int Y_ROTATE_PLUS = 7;
+	private final int Z_ROTATE_PLUS = 8;
+	private final int X_ROTATE_NEG  = 9;
+	private final int Y_ROTATE_NEG  = 10;
+	private final int Z_ROTATE_NEG  = 11;
+
+	// Rotation and Translation Input
+	//
+	// rotationKey
+	// translationKey
+	//
+	//                 (yPlusKey)
+	//                 +y     (zNegKey)
+	//                  |    -z
+	//                  |    /
+	//                  |   /
+	//                  |  /
+	//                  | /
+	// (xNegKey)        |/
+	//     -x ----------o---------- +x (yPlusKey)
+	//                 /|
+	//                / |
+	//               /  |
+	//              /   |
+	//             /    |
+	//           +z     |
+	//    (zPlusKey)   -y
+	//                 (yNegKey)
+	//
+	public Movement(Vector4f position, Vector4f lookAtPoint, Vector4f up,
+			int xPlusKey, int yPlusKey, int zPlusKey,
+			int xNegKey,  int yNegKey,  int zNegKey,
+			int translateKey, int rotationKey)
+	{
+		Transform transform = new Transform(position);
+		Quaternion quarternion = transform.GetLookAtRotation(lookAtPoint, up);
+		this.m_transform = transform.Rotate(quarternion);
+		m_keys[X_TRANS_PLUS] = new KeyInfo(xPlusKey, translateKey);
+		m_keys[Y_TRANS_PLUS] = new KeyInfo(yPlusKey, translateKey);
+		m_keys[Z_TRANS_PLUS] = new KeyInfo(zPlusKey, translateKey);
+		m_keys[X_TRANS_NEG ] = new KeyInfo(xNegKey,  translateKey);
+		m_keys[Y_TRANS_NEG ] = new KeyInfo(yNegKey,  translateKey);
+		m_keys[Z_TRANS_NEG ] = new KeyInfo(zNegKey,  translateKey);
+
+		m_keys[X_ROTATE_PLUS] = new KeyInfo(xPlusKey, rotationKey);
+		m_keys[Y_ROTATE_PLUS] = new KeyInfo(yPlusKey, rotationKey);
+		m_keys[Z_ROTATE_PLUS] = new KeyInfo(zPlusKey, rotationKey);
+		m_keys[X_ROTATE_NEG ] = new KeyInfo(xNegKey,  rotationKey);
+		m_keys[Y_ROTATE_NEG ] = new KeyInfo(yNegKey,  rotationKey);
+		m_keys[Z_ROTATE_NEG ] = new KeyInfo(zNegKey,  rotationKey);
+	}
+
+	public void Update(Input input, float translationDelta, float rotationDelta)
+	{
+		for (int i = 0; i < m_keys.length; i++) {
+			KeyInfo ki = m_keys[i];
+
+			if(ki.key1Pressed && ki.key2Pressed && (!input.GetKey(ki.key1) || !input.GetKey(ki.key2))) {
+				Dbg.p(String.format("i=%d key1=%d key2=%d pressed\n", i, ki.key1, ki.key2));
+				this.m_changed = true;
+				switch (i) {
+					case X_TRANS_PLUS: {
+						if(DBG) Dbg.p("trans x+\n");
+						Translate(m_transform.GetRot().GetRight(), translationDelta);
+						break;
+					}
+					case Y_TRANS_PLUS: {
+						if(DBG) Dbg.p("trans y+\n");
+						Translate(m_transform.GetRot().GetUp(), translationDelta);
+						break;
+					}
+					case Z_TRANS_PLUS: {
+						if(DBG) Dbg.p("trans z+\n");
+						Translate(m_transform.GetRot().GetForward(), translationDelta);
+						break;
+					}
+					case X_TRANS_NEG: {
+						if(DBG) Dbg.p("trans x-\n");
+						Translate(m_transform.GetRot().GetLeft(), translationDelta);
+						break;
+					}
+					case Y_TRANS_NEG: {
+						if(DBG) Dbg.p("trans y-\n");
+						Translate(m_transform.GetRot().GetDown(), translationDelta);
+						break;
+					}
+					case Z_TRANS_NEG: {
+						if(DBG) Dbg.p("trans z-\n");
+						Translate(m_transform.GetRot().GetBack(), translationDelta);
+						break;
+					}
+					case X_ROTATE_PLUS: {
+						if(DBG) Dbg.p("rotate x+\n");
+						Rotate(X_AXIS, rotationDelta);
+						break;
+					}
+					case Y_ROTATE_PLUS: {
+						if(DBG) Dbg.p("rotate y+\n");
+						Rotate(Y_AXIS, rotationDelta);
+						break;
+					}
+					case Z_ROTATE_PLUS: {
+						if(DBG) Dbg.p("rotate z+\n");
+						Rotate(Z_AXIS, rotationDelta);
+						break;
+					}
+					case X_ROTATE_NEG: {
+						if(DBG) Dbg.p("rotate x-\n");
+						Rotate(X_AXIS, -rotationDelta);
+						break;
+					}
+					case Y_ROTATE_NEG: {
+						if(DBG) Dbg.p("rotate y-\n");
+						Rotate(Y_AXIS, -rotationDelta);
+						break;
+					}
+					case Z_ROTATE_NEG: {
+						if(DBG) Dbg.p("rotate z-\n");
+						Rotate(Z_AXIS, -rotationDelta);
+						break;
+					}
+				}
+			}
+
+			if(!ki.key1Pressed && input.GetKey(ki.key1)) {
+				if(DBG) Dbg.p(String.format("key1=%d pressed\n", ki.key1));
+				ki.key1Pressed = true;
+			}
+			if(!ki.key2Pressed && input.GetKey(ki.key2)) {
+				if(DBG) Dbg.p(String.format("key2=%d pressed\n", ki.key2));
+				ki.key2Pressed = true;
+			}
+
+			if(ki.key1Pressed && !input.GetKey(ki.key1)) {
+				if(DBG) Dbg.p(String.format("key1=%d released\n", ki.key1));
+				ki.key1Pressed = false;
+			}
+			if(ki.key2Pressed && !input.GetKey(ki.key2)) {
+				if(DBG) Dbg.p(String.format("key2=%d released\n", ki.key2));
+				ki.key2Pressed = false;
+			}
+		}
+	}
+
+	public void Translate(Vector4f dir, float amt)
+	{
+		m_transform = m_transform.SetPos(m_transform.GetPos().Add(dir.Mul(amt)));
+	}
+
+	public void Rotate(Vector4f axis, float angle)
+	{
+		m_transform = m_transform.Rotate(new Quaternion(axis, angle));
+	}
+
+	boolean m_changed()
+	{
+		return this.m_changed;
+	}
+
+	Transform GetTransform()
+	{
+		this.m_changed = false;
+		return m_transform;
+	}
+}
